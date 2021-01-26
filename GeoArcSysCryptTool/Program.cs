eb48b721e5bbc96235f6e06bb5504423f38e886b
Geordan9 <geo3125@gmail.com>
2021-01-25 20:30:43 -0700

    Add project files.

---
+++ GeoArcSysCryptTool/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using ArcSysAPI.Common.Enums;
+using ArcSysAPI.Utils;
+
+namespace GeoArcSysCryptTool
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var file = @"C:\Users\Administrator\Downloads\char_am_img.pac";
+
+            using (var fileStream = File.Open(file, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+                using (var stream =
+                    BBObfuscatorTools.DFASFPACDeflateStream(fileStream, true))
+                {
+                    using (var decryptStream =
+                        BBObfuscatorTools.FPACCryptStream(stream, file, CryptMode.Encrypt))
+                    {
+                        fileStream.Close();
+                        File.WriteAllBytes(file, decryptStream.ToArray());
+                        decryptStream.Close();
+                    }
+                }
+                
+            }
+        }
+    }
+}
